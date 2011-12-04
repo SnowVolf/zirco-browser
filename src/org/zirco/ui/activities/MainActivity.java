9062a18b2289880cb9bded0684aeb075a9b8fa6a
j.Devauchelle <j.Devauchelle@e747efee-954d-ea6f-70a2-271fa72c5eea>
2011-12-04 09:59:51 +0000

    Branch for holo theme test

--- src/org/zirco/ui/activities/MainActivity.java
+++ src/org/zirco/ui/activities/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends Activity implements IToolbarsContainer, OnTouc
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	//this.setTheme(R.style.Dark);
+    	this.setTheme(R.style.Dark);
         super.onCreate(savedInstanceState);              
 
         INSTANCE = this;
@@ -494,16 +494,17 @@ public class MainActivity extends Activity implements IToolbarsContainer, OnTouc
     	
     	mUrlEditText.addTextChangedListener(mUrlTextWatcher);
     	
-    	mUrlEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-    		@Override
-    		public void onFocusChange(View v, boolean hasFocus) {
-    			// Select all when focus gained.
-    			if (hasFocus) {
-    				mUrlEditText.setSelection(0, mUrlEditText.getText().length());
-    			}
-    		}
-    	});    	
+//    	mUrlEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//
+//    		@Override
+//    		public void onFocusChange(View v, boolean hasFocus) {
+//    			// Select all when focus gained.
+//    			if (hasFocus) {
+//    				//mUrlEditText.setSelection(0, mUrlEditText.getText().length());
+//    				mUrlEditText.selectAll();
+//    			}
+//    		}
+//    	});    	
     	
     	mUrlEditText.setCompoundDrawablePadding(5);
     	    	
